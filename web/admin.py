187b3cc070911485d214e50f470dfd7b8c0bdae0
Negar Javadzadeh <47604409+NegarJvd@users.noreply.github.com>
2023-08-28 01:40:21 +0330

    adding urls and views for income and expose

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Expense, Income
+from .models import Token, Expense, Income
 
 # Register your models here.
 
+admin.site.register(Token)
 admin.site.register(Expense)
 admin.site.register(Income)
--- web/views.py
+++ web/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from .models import *
+from datetime import datetime
+
 
 # Create your views here.
+
+@csrf_exempt
+def submit_expense(request):
+
+    # TODO: validate data
+    token = request.POST['token']
+    this_user = User.objects.filter(token__token = token).get()
+    now = datetime.now()
+    Expense.objects.create(user=this_user, amount=request.POST['amount'], text=request.POST['text'], date=now)
+
+    return JsonResponse({
+        'status': 'ok'
+    }, encoder=JSONEncoder)
+
+
+@csrf_exempt
+def submit_income(request):
+
+    # TODO: validate data
+    token = request.POST['token']
+    this_user = User.objects.filter(token__token=token).get()
+    now = datetime.now()
+    Income.objects.create(user=this_user, amount=request.POST['amount'], text=request.POST['text'], date=now)
+
+    return JsonResponse({
+        'status': 'ok'
+    }, encoder=JSONEncoder)
